46fc80bd1a8051cf7b6c8b164b499c3f1d180882
Shilov Ilya <ShilovIlya@gmail.com>
2017-01-25 13:34:21 +0300

    Add File Manager.

--- src/main/java/ru/alee/software/test/AppStart.java
+++ src/main/java/ru/alee/software/test/AppStart.java
@@ -1,17 +1,32 @@
 package ru.alee.software.test;
 
 import org.apache.log4j.Logger;
+import ru.alee.software.test.model.FileManager;
+
+import java.io.File;
 
 public class AppStart {
 
-    final static Logger logger = Logger.getLogger(AppStart.class);
+    private final static Logger logger = Logger.getLogger(AppStart.class);
 
     public static void main(String[] args) {
 
         if (logger.isDebugEnabled()) {
             logger.debug("Application start.");
         }
-
+        FileManager fileManager = new FileManager();
+        FileManager fileManager2 = new FileManager("C://");
+        File file = new File("D://");
+        FileManager fileManager3 = new FileManager(file);
+        logger.debug("print default: ");
+        fileManager.printFileList();
+        fileManager.printFileAbsolutePathList();
+        logger.debug("print with C:// argument ");
+        fileManager2.printFileList();
+        fileManager2.printFileAbsolutePathList();
+        logger.debug("print with File(D://) argument ");
+        fileManager3.printFileList();
+        fileManager3.printFileAbsolutePathList();
 
     }
 }
